6422a160c5786f5abe8ae45cdd4474adf19341ac
Shubh-ujala <166637739+Shubh-ujala@users.noreply.github.com>
2024-05-31 15:20:10 +0000

    conversions of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let score = 33
+let numberScore = Number(score);
+
+console.log(numberScore);//33
+console.log(typeof numberScore);//number
+
+let score1 = "33abc"
+let numberScore1 = Number(score1);
+
+console.log(numberScore1);//NaN
+console.log(typeof numberScore1);//number
+
+/*
+    "33" => 33
+    "33abc" => NaN
+    true => 1 
+    false => 0
+*/
+let isLoggedIn = 1;
+let CheckisLoggedIn = Boolean(isLoggedIn);
+console.log(CheckisLoggedIn); // true
+console.log(typeof CheckisLoggedIn); // boolean
+
+let stringScore = String(score);
+console.log(stringScore);
+console.log(typeof stringScore);
