01b20d92ec702b185cfab5b0c7d56c5376191ac9
Shubh-ujala <166637739+Shubh-ujala@users.noreply.github.com>
2024-06-01 18:07:49 +0000

    string operations

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+let myName = "Shubh"
+// console.log(typeof myName);
+
+let myRepoCount = 10
+
+// console.log(myName+" "+myRepoCount);//old way not prefered generally
+
+// console.log(`Hi My name is ${myName} and my repo count in github is ${myRepoCount}.`);//prefer to use back ticks 
+
+const gameName = new String('Shubh- fff')//Way to declare the string
+console.log(gameName);
+console.log(typeof gameName); //object
+console.log(gameName.charAt(5));
+console.log(gameName.includes("Shubh"));
+console.log(gameName.length);
+console.log(gameName.indexOf('f'));
+console.log(gameName.indexOf('t'));
+console.log(gameName.split(' '));
+console.log(gameName.split('-'));
+// console.log(typeof gameName);
+console.log(gameName.endsWith("f"));
+console.log(gameName.replace("-","%&"));
+console.log(gameName);//old string will be as it is because it is stack iykyk
+
+
