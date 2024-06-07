bfa30b1245ee75747135b2e4b5f8deb89e3dcf0a
Shubh Ujala <166637739+Shubh-ujala@users.noreply.github.com>
2024-06-07 08:36:22 +0000

    while_and_dowhile done

---
+++ 05_Iterations/02_two.js
@@ -0,0 +1,21 @@
+let i = 0;
+while(i<=10){
+    // console.log(`Value of index is ${i}`);
+    i = i+2;
+}
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0;
+while(arr<myArray.length){
+    // console.log(`Value is ${myArray[arr]}`);
+    arr++;
+}
+
+let score = 14;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);
+//the major difference in while and do_while loop is in while loop when the condition is true then only the loop will be executed but in do_while loop it will execute ateast once even the condition is not true.
+
