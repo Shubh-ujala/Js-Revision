79eb8090f652b18d70b36053f8fa50e3d50f442a
Shubh Ujala <166637739+Shubh-ujala@users.noreply.github.com>
2024-06-07 15:10:38 +0000

    map_function discussed

---
+++ 05_Iterations/07_seven.js
@@ -0,0 +1,14 @@
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.map((numm)=>{
+//     return numm+1
+// }) //[2, 3, 4,  5,  6, 7, 8, 9, 10, 11]
+
+// const newNums = myNums.filter((numm) => {return numm + 1}) //[1,2,3,4,5,6,7,8,9,10]
+// console.log(newNums);
+
+const final_Nums = myNums
+                    .map((nums)=>nums*0)
+                    .map((nums)=>nums+10)
+                    .filter((nums)=> nums>8)
+console.log(final_Nums);
